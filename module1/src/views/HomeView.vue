490ef9f9e714326d387ba67ef8be9f7765a8764b
fernandobf <fernandobravofigueroa@gmail.com>
2022-12-24 13:07:02 +0000

    Start project

---
+++ module1/src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+  created() {
+    // alert("olá mundo");
+    // console.log("Teste no-console");
+  },
+};
+</script>
+<style lang="scss">
+.home {
+  img {
+    display: none;
+  }
+  background-color: #f00;
+}
+</style>
